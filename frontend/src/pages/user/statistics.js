d1e4bb08e6d1f181182a02c74167b66c1b26ae68
Ahmed <101567711+AhmedBSF@users.noreply.github.com>
2024-12-12 11:10:41 +0300

    fix: getMonths infinite API call

--- frontend/src/pages/user/statistics.js
+++ frontend/src/pages/user/statistics.js
@@ -1,41 +1,47 @@
 import IncomeVsExpenseChart from "../../components/userDashboard/incomeVsExpenseChart";
 import Header from "../../components/utils/header";
-import Loading from '../../components/utils/loading';
-import useExpenseVsIncomeSummary from '../../hooks/useExpenseVsIncomeSummary';
+import Loading from "../../components/utils/loading";
+import useExpenseVsIncomeSummary from "../../hooks/useExpenseVsIncomeSummary";
 import Info from "../../components/utils/Info";
 import Container from "../../components/utils/Container";
 import toast, { Toaster } from "react-hot-toast";
+import { useMemo } from "react";
 
 function UserStatistics() {
-    const months = getMonths()
-    const [data, isLoading, isError] = useExpenseVsIncomeSummary(months)
+  const months = useMemo(() => getMonths(), []);
 
-    return (
-        <Container activeNavId={9}>
-            <Header title="Statistics" />
-            <Toaster/>
-            {(isLoading) && <Loading />}
-            {(isError) && toast.error("Failed to fetch information. Try again later!") }
-            {(isError) && <Info text="No data found!" />}
-            {(!isError) && <IncomeVsExpenseChart data={data} />}
-        </Container>
-    )
+  const [data, isLoading, isError] = useExpenseVsIncomeSummary(months);
+
+  return (
+    <Container activeNavId={9}>
+      <Header title="Statistics" />
+      <Toaster />
+      {isLoading && <Loading />}
+      {isError && toast.error("Failed to fetch information. Try again later!")}
+      {isError && <Info text="No data found!" />}
+      {!isError && <IncomeVsExpenseChart data={data} />}
+    </Container>
+  );
 }
 
 export default UserStatistics;
 
 function getMonths() {
-    const months = []
-    const current_date = new Date()
+  const months = [];
+  const current_date = new Date();
 
-    for (let i = 11; i >= 0; i--) {
-        const date = new Date(current_date.getFullYear(), current_date.getMonth() - i, 1)
-        months.push({
-            id: date.getMonth() + 1,
-            year: date.getFullYear(),
-            monthName: date.toLocaleString('en-US', { month: 'long' })
-        })
-    }
+  for (let i = 11; i >= 0; i--) {
+    const date = new Date(
+      current_date.getFullYear(),
+      current_date.getMonth() - i,
+      1
+    );
+    months.push({
+      id: date.getMonth() + 1,
+      year: date.getFullYear(),
+      monthName: date.toLocaleString("en-US", { month: "long" }),
+    });
+  }
 
-    return months
+  return months;
 }
